8018db117655280f05d9bd1d532ff8637216d990
ColeLMII <97560688+ColeLMII@users.noreply.github.com>
2022-04-25 10:49:02 -0500

    Update app.js

--- SwollenCoffee/js/app.js
+++ SwollenCoffee/js/app.js
@@ -31,7 +31,7 @@ $(document).on('click','#btnLogin', function(){
             }
         })
     } else{
-        $.post('http://localhost:7071/api/swollenCoffee?',{function:'session', strEmail:$('#txtEmail').val(),strPassword:$('#txtPassword').val()},function(result){
+        $.getJSON('http://localhost:7071/api/swollenCoffee',{function:'session', Email:$('#txtEmail').val(),Password:$('#txtPassword').val()},function(result){
         objResult = JSON.parse(result); 
         
         if(objResult.Outcome != 'Login Failed'){
@@ -39,7 +39,7 @@ $(document).on('click','#btnLogin', function(){
                 sessionStorage.setItem('MembershipID', objResult.Outcome);
                 // then redirect the user to the dashboard
                 window.location.href='index.html';
-                fillCharacterTable();
+                fillPurchaseHistoryTable();
             } else {
                 Swal.fire({
                     icon: 'error',
@@ -165,7 +165,7 @@ $(document).on('click','#btnSignOut', function(){
     let strErrorMessage = '';
     
     window.location.href='login.html';
-        $.post('http://localhost:7071/api/swollenCoffee?',{function:'session',strEmail:$('#txtEmail').val(),strPassword:$('#txtPassword').val()},function(result){
+        $.post('http://localhost:7071/api/swollenCoffee',{function:'session',strEmail:$('#txtEmail').val(),strPassword:$('#txtPassword').val()},function(result){
         objResult = JSON.parse(result); //should update last used date and time
         
         if(objResult.Outcome != 'Login Failed'){
@@ -196,24 +196,98 @@ $(document).on('click','#btnUpdateInformation', function(){
 $(document).on('click', '#btnSubmitUpdate', function(){
     let strErrorMessage='';
     let blnError = false;
-
-    if(!$('#txtNewEmail').val()){
-        blnError = true;
-        strErrorMessage += '<p> New Email Address is empty.</p>';
+    if(!$('#txtEmail').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Email is Blank.</p>';
+    }else if(!isValidEmail($('#txtEmail').val())){
+        blnError=true;
+        strErrorMessage+='<p>Email is not valid</p>';
     }
-    
+    if(!$('#txtPassword').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Password is Blank.</p>';
+    }else if(!isValidPassword($('#txtPassword').val())){
+        blnError=true;
+        strErrorMessage+='<p>Password is not valid</p>';
+    }
+    if(!$('#txtFirstName').val()){
+        blnError=true;
+        strErrorMessage+= '<p>First Name is Blank.</p>';
+    }
+    if(!$('#txtLastName').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Last Name is Blank.</p>';
+    }
+    if(!$('#txtAddress1').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Address 1 is Blank.</p>';
+    }
+    if(!$('#txtAddress2').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Address 2 is Blank.</p>';
+    }
+    if(!$('#txtCity').val()){
+        blnError=true;
+        strErrorMessage+= '<p>City is Blank.</p>';
+    }
+    if(!$('#txtState').val()){
+        blnError=true;
+        strErrorMessage+= '<p>State is Blank.</p>';
+    }
+    if(!$('#txtZIP').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Zip Code is Blank.</p>';
+    } else if ($('#txtZIP').val().length < 5 || $('#txtZIP').val().length < 5){
+        blnError=true;
+        strErrorMessage+= '<p>Zip Code Must be 5 Digits Long.'
+    }
+    if(!$('#txtPhoneNumber').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Phone Number is Blank.</p>';
+    }
+    if(!$('#txtDateOfBirth').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Birthday is Blank.</p>';
+    }
     if(blnError == true){
         Swal.fire({
-            icon: 'warning',
-            title: 'Unable to Update',
+            icon: 'error',
+            title: 'Missing Data',
             html: strErrorMessage
+        })
+    }else{ // this needs to be a put not a post.
+        var objNewSessionPromise= $.post('http://localhost:7071/api/swollenCoffee?', {function:'membership',Email:$('#txtEmail').val(), strFirstName:$('#txtFirstName').val(), strLastName:$('#txtLastName').val(), strPassword:$('#txtPassword').val(), strAddress1:$('txtAddress1').val(),strAddress2:$('txtAddress2').val(),strCity:$('txtCity').val(),strState:$('txtState').val(),strZip:$('txtZIP').val(),strPhoneNumber:$('#txtPhoneNumber').val(),strDateOfBirth:$('#txtDateOfBirth').val()}, function(result){
+            console.log(JSON.parse(result).Outcome);
+            objNewSessionPromise = JSON.parse(result);
+        }) 
+        $.when(objNewSessionPromise).done(function(){
+            if(objNewSessionPromise.Outcome == 'Error'){
+                Swal.fire({
+                    icon:'error',
+                    title:'Login Failed',
+                    html: '<p> Review Username and Password </p>'
+                })
+            } else{ 
+                console.log(objNewSessionPromise);//for error checking
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Update Successful!',
+                    showClass: {
+                        popup: `
+                        animate__animated
+                        animate__fadeInDown
+                        animate__faster
+                        `
+                    }
+                })
+            }
         })
     }
 })
 
 $(document).on('click','#dropdownPrefLocation', function(){
     let strCurrentSessionID = sessionStorage.getItem('MembershipID');
-    $.getJSON('http://localhost:7071/api/swollencoffee?function=location',{strSessionID: strCurrentSessionID}, function(result){
+    $.getJSON('http://localhost:7071/api/swollencoffee',{function:'location', SessionID: strCurrentSessionID}, function(result){
         console.log(result);
         $.each(arrLocation, function(i,location){
             console.log(location.LocationID);
@@ -233,7 +307,7 @@ $(document).on('click','#btnViewHistory',function(){
     allows user to click a transaction id and produces a sweetalert
 */
 $(document).on('click','#btnTransaction', function(){
-    let strTransID=$(this).attr();
+    let strTransID=$(this).attr('TransactionID');
 
     Swal.fire({
         icon: 'info',
@@ -249,16 +323,14 @@ $(document).on('click','#btnTransaction', function(){
 
 function fillPurchaseHistoryTable(){
     $('#tblPurchaseHistory tbody').empty();
-    /*let strCurrentSessionID = sessionStorage.getItem('MembershipID');
-            let strTableHTML = '<tr><td> <button type="button" class="btn col-12" id="btnTransaction">  x42ghue78 </button></td></tr>';
+    let strCurrentSessionID = sessionStorage.getItem('MembershipID');
+            /*let strTableHTML = '<tr><td> <button type="button" class="btn col-12" id="btnTransaction">  x42ghue78 </button></td></tr>';
             console.log("here");
             $('#tblPurchaseHistory tbody').append(strTableHTML);*/
-    $.getJSON('http://localhost:7071/api/swollenCoffee?function=purchases',{strSessionID:strCurrentSessionID},function(result){
+    $.getJSON('http://localhost:7071/api/swollenCoffee',{function: 'purchases', SessionID:strCurrentSessionID},function(result){
         $.each(result,function(i,transaction){
             let strTableHTML = '<tr><td> <button type="button" class="btn" id="btnTransaction"> '+  transaction.TransactionID+'</button></td></tr>';
             $('#tblPurchaseHistory tbody').append(strTableHTML);
-
-
         })
         })
 }
@@ -292,7 +364,7 @@ function doPasswordsMatch(strPassword, strVerifyPassword){
 function verifySession(){
     if(sessionStorage.getItem('MembershipID')){
         let strCurrentSessionID = sessionStorage.getItem('MembershipID')
-        $.getJSON('http://localhost:7071/api/swollenCoffee?', {function:'session', strSessionID: strCurrentSessionID}, function(result){
+        $.getJSON('http://localhost:7071/api/swollenCoffee', {function:'session', strSessionID: strCurrentSessionID}, function(result){
             if(result.Outcome != 'Valid Session'){
                 return false;
             } else {
